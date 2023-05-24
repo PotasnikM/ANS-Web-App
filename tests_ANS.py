1ae2a29ad21465be8e0bd9e14931bf058a7ee5e4
Mateusz Potaśnik <nnn.2321a@gmail.com>
2023-05-24 16:41:50 +0200

    update rANS

--- tests_ANS.py
+++ tests_ANS.py
@@ -1,6 +1,5 @@
 import random
 import math
-from ANS.ANS_rescaling import ans_decoder_rescaling, ans_encoder_rescaling
 from ANS.ANS_no_rescaling import ans_decoder_no_rescaling, ans_encoder_no_rescaling
 
 # initialize data distribution and input length
@@ -15,25 +14,8 @@ sequence_length = 10000
 random.seed(2)
 symbols = random.choices(range(len(p)), weights=p, k=sequence_length)
 
-# encode
-s, t_stack = ans_encoder_rescaling(symbols, p, c, r, r_s, r_t)
-
-# decode
-decoded_symbols = ans_decoder_rescaling(s, t_stack[:], p, c, r, r_s, r_t)
-
 # statistics
-average_bps = (r_s + len(t_stack) * r_t) / sequence_length
 entropy = sum(-i / 2 ** r * math.log2(i / 2 ** r) for i in p)
-
-# sanity check
-assert all(x == y for x, y in zip(decoded_symbols, symbols))
-
-# display results
-print(f"data source entropy    : {entropy:.5f} bits/symbol")
-print(f"tstact{t_stack}")
-print(f"s{s}")
-print("ANS with rescaling")
-print(f"average bits per symbol: {average_bps:.5f} bits/symbol")
 
 # encode
 s = ans_encoder_no_rescaling(symbols)
