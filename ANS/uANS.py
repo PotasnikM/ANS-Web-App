9c2fc99abe9ce267bb48d6bc5d25038a4920e360
RlCK <kcpr.hk@gmail.com>
2023-05-31 08:38:55 +0200

    uANS works

--- ANS/uANS.py
+++ ANS/uANS.py
@@ -2,26 +2,29 @@ from math import ceil, floor
 import numpy as np
 
 def uans_encode(msg):
-    p = 0.3
+    msg_dec = [int(x) for x in msg]
+    p1 = 0.3
     x = 1
-    for s in msg:
+    for s in msg_dec:
         if s == 0:
-            x = ceil((x+1)/(1-p)) - 1
+            x = ceil((x+1)/(1-p1)) - 1
         else:
-            x = floor(x/p)
+            x = floor(x/p1)
+        print(x)
     return x
 
 def uans_decode(encoded_msg):
     encoded_msg = int(encoded_msg)
-    p = 0.3
     msg = []
+    p1=0.3
+
     while encoded_msg > 1:
-        s = ceil((encoded_msg+1)*p) - ceil(encoded_msg*p)
+        s = ceil((encoded_msg+1)*p1) - ceil(encoded_msg*p1)
         msg.append(s)
         if s == 0:
-            encoded_msg = encoded_msg - ceil(encoded_msg*p)
+            encoded_msg = encoded_msg - ceil(encoded_msg*p1)
         else:
-            encoded_msg = ceil(encoded_msg*p)
+            encoded_msg = ceil(encoded_msg*p1)
     return ''.join(map(str, reversed(msg)))
 
 if __name__ == '__main__':
